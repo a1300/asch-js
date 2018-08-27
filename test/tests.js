4f944266063c98843a18b903c037feef968afae5
a1300 <matthias-knopp@gmx.net>
2018-08-27 19:10:57 +0200

    Add jest - remove mocha

--- test/tests.js
+++ test/tests.js
@@ -21,8 +21,6 @@ describe("Asch JS", () => {
 		});
 	});
 
-
-
 	describe('crypto sha256 and address', () => {
 		it('should be equal to the expected address', () => {
 			asch.crypto.getAddress('7a91b9bfc0ea185bf3ade9d264da273f7fe19bf71008210b1d7239c82dd3ad20').should.be.equal('AFbYJhiJb3DXzHy5ZP24mKw21M2dCBJCXP')
@@ -30,5 +28,4 @@ describe("Asch JS", () => {
 			asch.crypto.getAddress(publicKeyBuffer).should.be.equal('AFbYJhiJb3DXzHy5ZP24mKw21M2dCBJCXP')
 		})
 	})
-
 });
