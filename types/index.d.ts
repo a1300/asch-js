c2e841db1a6d7350c8ca42b09a352394fd41da23
a1300 <matthias-knopp@gmx.net>
2018-09-01 20:05:21 +0200

    Add typescript support

---
+++ types/index.d.ts
@@ -0,0 +1,218 @@
+import * as Buffer from "buffer";
+
+/*
+  crypto.js
+*/
+interface KeyBuffer {
+  keypair: {
+    publicKey: Uint8Array;
+    secretKey: Uint8Array;
+  };
+  publicKey?: string;
+  privateKey?: string;
+}
+
+declare enum SendingMode {
+  DIRECT = 0,
+  REQUEST = 1
+}
+
+interface MainchainTransaction {
+  type: number;
+  timestamp: number;
+  fee: number;
+  message?: string;
+  args: any[];
+  senderPublicKey?: string;
+  senderId: string;
+  signatures?: string[];
+  id?: string;
+  secondSignature?: string;
+  requestorId?: string;
+  height?: number;
+  mode?: SendingMode;
+}
+
+interface Crypto {
+  getBytes: (transaction: MainchainTransaction, skipSignature?: boolean, skipSecondSignature?: boolean) => ArrayBuffer | Uint8Array;
+  getHash: (transaction: MainchainTransaction, skipSignature?: boolean, skipSecondSignature?: boolean) => Uint8Array;
+  getId: (transaction: MainchainTransaction) => string;
+  getFee: (transaction: MainchainTransaction) => number;
+  sign: (transaction: any, keys: any) => any;
+  secondSign: (transaction: MainchainTransaction, keys: KeyBuffer) => string;
+  getKeys: (secret: string) => KeyBuffer;
+  getAddress: (publicKey: string | Uint8Array) => string;
+  verify: (transaction: MainchainTransaction) => boolean;
+  verifySecondSignature: (transaction: MainchainTransaction, publicKey: string) => boolean;
+  fixedPoint: number;
+  signBytes: (bytes: any, keys: any)  => any;
+  toLocalBuffer: (buf: Buffer) => ArrayBuffer | Uint8Array;
+  verifyBytes: (bytes: string, signature: string, publicKey: string) => boolean;
+  isAddress: (address: string) => boolean;
+  isBase58CheckAddress: (address: string) => boolean;
+}
+
+/*
+  dapp.js
+*/
+interface DappOptions {
+  name: string;
+  desc: string;
+  link: string;
+  icon: string;
+  delegates: string[];
+  unlockDelegates: number;
+}
+
+interface InnerTransactionOptions {
+  args: any[];
+  fee: string;
+  type: number;
+}
+
+interface SidechainTransaction {
+  fee: string;
+  timestamp: number;
+  senderPublicKey: string;
+  type: number;
+  args: any[];
+  signature: string;
+}
+
+interface Dapp {
+  createDApp: (options: DappOptions, secret: string, secondSecret?: string) => MainchainTransaction;
+  createInnerTransaction: (options: InnerTransactionOptions, secret: string) => SidechainTransaction;
+}
+
+/*
+ basic.js
+*/
+interface Basic {
+  setName: (username: string, secret: string, secondSecret?: string) => MainchainTransaction;
+}
+
+/*
+  transfer.js
+*/
+interface Transfer {
+  createInTransfer: (dappName: string, currency: string, amount: string, secret: string, secondSecret?: string) => MainchainTransaction;
+  createOutTransfer: (sender: string, chain: string, recipient: string, currency: string, amount: string, wid: string, seq: number) => MainchainTransaction;
+  signOutTransfer: (transaction: MainchainTransaction, secret: string) => string;
+}
+
+/*
+  delegate.js
+*/
+interface Delegate {
+  createDelegate: (secret: string, secondSecret?: string) => MainchainTransaction;
+}
+
+/*
+  vote.js
+*/
+interface Vote {
+  createVote: (keyList: string[], secret: string, secondSecret?: string) => MainchainTransaction;
+  deleteVote: (keyList: string[], secret: string, secondSecret?: string) => MainchainTransaction;
+}
+
+/*
+  signature.js
+*/
+interface signature {
+  createSignature: (secret: string, secondSecret: string) => MainchainTransaction;
+}
+
+/*
+  transaction.js
+*/
+interface createTransactionExParams {
+  secret: string;
+  type: number;
+  fee: number;
+  message?: string;
+  args: string[];
+  secondSecret?: string;
+}
+
+interface multiSigTransactionParams {
+  type: number;
+  fee: number;
+  senderId: string;
+  requestId: string;
+  mode: SendingMode;
+  args: any[];
+}
+
+interface transaction {
+  createTransaction: (recipientId: string, amount: string, message: string, secret: string, secondSecret?: string) => MainchainTransaction;
+  createTransactionEx: (params: createTransactionExParams) => MainchainTransaction;
+  calculateFee: (amount: number) => number;
+  createLock: (height: number, amount: number, secret: string, secondSecret?: string) => MainchainTransaction;
+  unlock: (secret: string, secondSecret?: string) => MainchainTransaction;
+  createMultiSigTransaction: (parmas: multiSigTransactionParams) => any;
+  signMultiSigTransaction: (transaction: any, secret: string) => any;
+}
+
+/*
+  uia.js
+*/
+interface uia {
+  createIssuer: (name: string, desc: string, secret: string, secondSecret?: string) => MainchainTransaction;
+  createAsset: (name: string, desc: string, maximum: number, precision: number, secret: string, secondSecret?: string) => MainchainTransaction;
+  createIssue: (currency: string, amount: string, secret: string, secondSecret?: string) => MainchainTransaction;
+  createTransfer: (currency: string, amount: string, recipientId: string, message: string, secret: string, secondSecret?: string) => MainchainTransaction;
+}
+
+/*
+  options.js
+*/
+interface options {
+  set: (key: string, value: any) => void;
+  get: (key: string) => any;
+  getAll: () => any[];
+}
+
+/*
+  slots.js
+*/
+interface slots {
+	interval: number;
+	delegates: number;
+	getTime: (time: number) => number;
+	getRealTime: (epochTime?: string) => number;
+	getSlotNumber: (epochTime?: number) => number;
+	getSlotTime: (slot: number) => number;
+	getNextSlot: () => number;
+	getLastSlot: (nextSlot: string) => number;
+	beginEpochTime: () => number;
+}
+
+/*
+  format.js
+*/
+interface format {
+  timeAgo: (time: number) => string;
+  fullTimestamp: (time: number) => string;
+}
+
+/*
+  asch-js
+*/
+export = asch_js;
+
+declare const asch_js: {
+  basic: Basic,
+  crypto: Crypto,
+  dapp: Dapp,
+  transfer: Transfer,
+  delegate: Delegate,
+  signature: signature,
+  transaction: transaction,
+  vote: Vote,
+  uia: uia,
+  options: options,
+  utils: {
+    slots: slots,
+    format: format
+  }
+};
